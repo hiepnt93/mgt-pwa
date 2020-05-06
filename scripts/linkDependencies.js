734bf1cb5a918fca3059b65400db7a6a75f92bf3
Hiep <hiepnt93@gmail.com>
2020-05-06 17:38:42 +0700

    scripts

---
+++ scripts/linkDependencies.js
@@ -0,0 +1,113 @@
+/**
+ * Setup core dependencies as they're not published yet
+ */
+
+const execa = require("execa");
+const path = require("path");
+
+const apiClientDir = path.resolve(__dirname, "../packages/api-client");
+const composablesDir = path.resolve(__dirname, "../packages/composables");
+const defaultThemeDir = path.resolve(__dirname, "../packages/theme");
+// const nuxtModuleDir = path.resolve(__dirname, "../packages/nuxt-module");
+// const helpersDir = path.resolve(__dirname, "../packages/helpers");
+// const cliDir = path.resolve(__dirname, "../packages/cli");
+// const commonsDir = path.resolve(__dirname, "../packages/commons");
+
+async function run() {
+  /**
+   * Link shopware-6-client
+   */
+  await execa("yarn", ["link"], {
+    stdio: "inherit",
+    cwd: apiClientDir
+  });
+  await execa("yarn", ["link", "@mgt-pwa/api-client"], {
+    stdio: "inherit",
+    cwd: defaultThemeDir
+  });
+
+  /**
+   * Link composables
+   */
+  await execa("yarn", ["link"], {
+    stdio: "inherit",
+    cwd: composablesDir
+  });
+  await execa("yarn", ["link", "@mgt-pwa/composables"], {
+    stdio: "inherit",
+    cwd: defaultThemeDir
+  });
+
+  // /**
+  //  * Link helpers
+  //  */
+  // await execa("yarn", ["link"], {
+  //   stdio: "inherit",
+  //   cwd: helpersDir
+  // });
+  // await execa("yarn", ["link", "@shopware-pwa/helpers"], {
+  //   stdio: "inherit",
+  //   cwd: defaultThemeDir
+  // });
+  
+  // /**
+  //  * Link commons
+  //  */
+  // await execa("yarn", ["link"], {
+  //   stdio: "inherit",
+  //   cwd: commonsDir
+  // });
+  // await execa("yarn", ["link", "@shopware-pwa/commons"], {
+  //   stdio: "inherit",
+  //   cwd: apiClientDir
+  // });
+  // await execa("yarn", ["link", "@shopware-pwa/commons"], {
+  //   stdio: "inherit",
+  //   cwd: helpersDir
+  // });
+  // await execa("yarn", ["link", "@shopware-pwa/commons"], {
+  //   stdio: "inherit",
+  //   cwd: composablesDir
+  // });
+
+
+  // /**
+  //  * Link nuxt-module
+  //  */
+  // await execa("yarn", ["link"], {
+  //   stdio: "inherit",
+  //   cwd: nuxtModuleDir
+  // });
+
+  // /**
+  //  * Link CLI dir
+  //  */
+  // await execa("yarn", ["link"], {
+  //   stdio: "inherit",
+  //   cwd: cliDir
+  // });
+
+  // await execa("yarn", ["link", "@shopware-pwa/nuxt-module"], {
+  //   stdio: "inherit",
+  //   cwd: cliDir
+  // });
+
+  /**
+   * Link default-theme dir
+   */
+  await execa("yarn", ["link"], {
+    stdio: "inherit",
+    cwd: defaultThemeDir
+  });
+
+
+  /**
+   * link local storefront-ui
+   */
+  // await execa("yarn", ["link", "@storefront-ui/vue"], {
+  //   stdio: "inherit",
+  //   cwd: defaultThemeDir
+  // });
+}
+
+run();
---
+++ scripts/utils.js
@@ -0,0 +1,49 @@
+const fs = require("fs");
+const chalk = require("chalk");
+
+const ownBuildProcessPackages = ["cli", "theme", "commons"];
+
+const allTargets = (exports.allTargets = fs
+  .readdirSync("packages")
+  .filter((f) => !!fs.statSync(`packages/${f}`).isDirectory()));
+
+const targets = (exports.targets = allTargets.filter((f) => {
+  if (
+    !fs.statSync(`packages/${f}`).isDirectory() ||
+    ownBuildProcessPackages.includes(f)
+  ) {
+    return false;
+  }
+  const pkg = require(`../packages/${f}/package.json`);
+  if (pkg.private && !pkg.buildOptions) {
+    return false;
+  }
+  return true;
+}));
+
+exports.fuzzyMatchTarget = (partialTargets, includeAllMatching) => {
+  const matched = [];
+  partialTargets.forEach((partialTarget) => {
+    for (const target of targets) {
+      if (target.match(partialTarget)) {
+        matched.push(target);
+        if (!includeAllMatching) {
+          break;
+        }
+      }
+    }
+  });
+  if (matched.length) {
+    return matched;
+  } else {
+    console.log();
+    console.error(
+      `  ${chalk.bgRed.white(" ERROR ")} ${chalk.red(
+        `Target ${chalk.underline(partialTargets)} not found!`
+      )}`
+    );
+    console.log();
+
+    process.exit(1);
+  }
+};
